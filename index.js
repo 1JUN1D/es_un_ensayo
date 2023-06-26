7b0d0d75502ebaa3a472affdbf8ca1ca6d3fd6a2
1JUN1D <127701099+1JUN1D@users.noreply.github.com>
2023-06-26 08:11:47 -0500

    .2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log("El JavaScript se está cargando correctamente");
+
 document.addEventListener('DOMContentLoaded', (event) => {
     var map = L.map('map').setView([51.505, -0.09], 13);
 
@@ -6,29 +8,72 @@ document.addEventListener('DOMContentLoaded', (event) => {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    // Agregamos el geocoder
     var geocoder = L.Control.geocoder().addTo(map);
-
-    // Movemos el geocoder al div #ubicacion
     document.getElementById('ubicacion').appendChild(geocoder.getContainer());
 
-    document.querySelector('#boton1').addEventListener('click', () => {
-        const formContainer = document.querySelector('#form_container');
-        if (formContainer.style.display === 'none' || formContainer.style.display === '') {
-            formContainer.style.display = 'block';
+    const formContainerElec = document.querySelector('#form_container_elec');
+    const formContainerMec = document.querySelector('#form_container_mec');
+    const continueBtn = document.getElementById('continue-btn');
+    let optionSelected = false;
+    let locationSet = false;
+
+    const checkContinue = () => {
+        if (optionSelected && locationSet) {
+            continueBtn.style.display = 'block';
         } else {
-            formContainer.style.display = 'none';
+            continueBtn.style.display = 'none';
+        }
+    };
+
+    const toggleFormContainerElectricista = () => {
+        if (formContainerElec.style.display === 'none' || formContainerElec.style.display === '') {
+            formContainerElec.style.display = 'block';
+        } else {
+            formContainerElec.style.display = 'none';
+        }
+        formContainerMec.style.display = 'none';
+    };
+
+    const toggleFormContainerMecanico = () => {
+        if (formContainerMec.style.display === 'none' || formContainerMec.style.display === '') {
+            formContainerMec.style.display = 'block';
+        } else {
+            formContainerMec.style.display = 'none';
+        }
+        formContainerElec.style.display = 'none';
+    };
+
+    document.querySelector('#boton1').addEventListener('click', toggleFormContainerElectricista);
+    document.querySelector('#boton2').addEventListener('click', toggleFormContainerMecanico);
+
+    document.querySelectorAll('#opciones_form_elec input[type="radio"], #opciones_form_mec input[type="radio"]').forEach(option => {
+        option.addEventListener('change', () => {
+            optionSelected = true;
+            checkContinue();
+        });
+    });
+
+    geocoder.on('markgeocode', function(e) {
+        locationSet = true;
+        checkContinue();
+    });
+
+    let serviceText = document.querySelector('#elegir_servicio h2');
+    let originalText = serviceText.textContent;
+
+    document.querySelector('#opciones button:nth-child(1)').addEventListener('click', () => {
+        if (serviceText.textContent === 'Servicio eléctrico') {
+            serviceText.textContent = originalText;
+        } else {
+            serviceText.textContent = 'Servicio eléctrico';
         }
     });
 
-
-
-    document.querySelectorAll('#opciones_form .form_option').forEach(option => {
-        option.addEventListener('click', () => {
-            document.querySelectorAll('#opciones_form .form_option').forEach(opt => {
-                opt.classList.remove('selected');
-            });
-            option.classList.add('selected');
-        });
+    document.querySelector('#opciones button:nth-child(2)').addEventListener('click', () => {
+        if (serviceText.textContent === 'Servicio mecánico') {
+            serviceText.textContent = originalText;
+        } else {
+            serviceText.textContent = 'Servicio mecánico';
+        }
     });
 });
